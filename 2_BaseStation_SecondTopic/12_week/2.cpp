68ba70f5632b92640b29bb6e84695df2bdbeb718
unknown <ian52759@gmail.com>
2024-01-20 01:12:51 +0800

    2024/1/20

---
+++ 2_BaseStation_SecondTopic/12_week/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    if(rand()%2==0){
+        // 跑n^2的演算法
+    }else{
+        // 跑n的演算法
+    }
+} 
---
+++ 2_BaseStation_SecondTopic/12_week/hw/2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void selection_sort(double arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    int min_idx = i;
+    for (int j = i + 1; j < n; j++)
+      if (arr[j] < arr[min_idx]) 
+      	min_idx = j;
+      swap(arr[min_idx], arr[i]); 
+  }
+}
+
+int main() {
+    double crank[1001]={0};
+    double wheel[1001]={0};
+    int n, m; double p;
+    cin >> n >> m >> p;
+    
+    p*=0.01;
+    // 處理輸入
+    for(int i=0; i<n; i++)
+        cin >> crank[i];
+    for(int i=0; i<m; i++)
+        cin >> wheel[i];
+
+    double record[1001]={0};
+    int count = 0;
+    // 把每一種配對的節奏記錄起來，稍後第39行會去判斷符不符合題目要求
+    for(int i=0; i < n; i++){
+        for(int j=0; j < m; j++){
+            record[count++] = double(wheel[j]/crank[i]);
+        }
+    }
+    // 因為變速可以調任和速度，意味著可以直接sort好，再從小到大看可不可以達成題目敘述
+    selection_sort(record, count);
+
+    // 若後一個前後齒輪配對所形成的節奏與前一個配對的比例關係大於p，直接輸出要換齒輪，並return 0;
+    for(int i=0; i<count-1; i++){
+        if((record[i+1]/record[i])-1 > p){
+            cout << "Time to change gears!" <<'\n';
+            return 0;
+        }
+    }
+    cout << "Ride on!" << '\n';
+    return 0;
+}
